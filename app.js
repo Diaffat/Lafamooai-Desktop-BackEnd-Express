857f846950c2703a4f96f2b08c44209cedb54f43
DIAFFAT <fatoumatasoukouradiassana18@gmail.com>
2026-07-02 13:04:13 +0100

    je change la logique metier

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const path = require('path');
 
 const app = express();
 
+app.use(cors({
+  origin: [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000"
+  ],
+  credentials: true,
+}));
+
+
 app.use(express.json());
-app.use(cors());
 
 // Serve static files from media directory
 app.use('/media', express.static(path.join(__dirname, 'media')));
@@ -13,6 +21,10 @@ app.use('/media', express.static(path.join(__dirname, 'media')));
 const routes = require('./routes');
 app.use('/', routes);
 
+app.get("/health", (req, res) => {
+  res.json({ ok: true });
+});
+
 app.listen(8000, () => {
-  console.log('Server running on http://localhost:8000');
+  console.log('Server running on express backend lafamooai http://127.0.0.1:8000');
 });
